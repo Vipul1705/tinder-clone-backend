ec9872485b264f06acfc975d41c6435885d75b5e
vipul patil <vipulpatil1999@gmail.com>
2023-01-07 13:35:10 +0530

    first commit

---
+++ dbCards.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const cardSchema = mongoose.Schema(
+    {
+        name: String,
+        imgUrl: String
+    }
+)
+
+export default mongoose.model('cards',cardSchema)
---
+++ server.js
@@ -0,0 +1,63 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Cors from 'cors'
+import Cards from './dbCards.js'
+
+//App Config
+const app=express()
+const port=process.env.PORT || 8001
+const connection_url = "mongodb+srv://<username>:<password>@cluster0.zubqjp5.mongodb.net/tinderdb?retryWrites=true&w=majority"
+
+//Middleware
+app.use(express.json())
+app.use(Cors())
+
+//DB config
+mongoose.connect(connection_url,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err)=>{
+        if(!err){
+            console.log("Database Connected");
+        }else{
+            console.log(err);
+        }
+    }
+)
+
+
+
+//API endpoint
+app.get('/',(req,res)=>{
+    res.status(200).send("Hello Backend Guys")
+})
+
+app.post('/tinder/cards',(req,res)=>{
+    const dbCard=req.body
+    Cards.create(dbCard,(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards',(req,res)=>{
+    Cards.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+//Listener
+app.listen(port,()=>{
+    console.log(`Listening on localhost: ${port}`);
+})
